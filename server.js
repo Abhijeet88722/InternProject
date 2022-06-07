d933423ac8946d23304318a16f2eab35d1d6c5c4
Abhijeet88722 <104162745+Abhijeet88722@users.noreply.github.com>
2022-06-07 17:38:57 +0530

    third commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const { appendFile } = require("fs");
+const mongoose = require("mongoose");
+
+app.listen(3000, function(){
+    console.log("server runnning");
+})
