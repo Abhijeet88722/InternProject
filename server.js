cb59ae744640591970204ccc038a729153e83833
Abhijeet88722 <104162745+Abhijeet88722@users.noreply.github.com>
2022-06-08 15:19:44 +0530

    fourth commit

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
-const bodyParser = require("body-parser");
 const express = require("express");
-const { appendFile } = require("fs");
+const app = express();
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { send } = require("process");
 
-app.listen(3000, function(){
-    console.log("server runnning");
+
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://abhijeetbasfore:abhijeet123@cluster0.pz4ax.mongodb.net/", {useNewgitUrlParser: true},{useUnifiedTopology: true});
+
+const infoSchema = {
+    name: String,
+    email : String,
+    phone: Number,
+    message: String
+}
+
+const Info = mongoose.model("Info", infoSchema)
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    let newInfo = new Info ({
+        name: req.body.name,
+        email:req.body.email,
+        phone:req.body.phone,
+        message:req.body.message
+    });
+    newInfo.save();
+    res.redirect('/');
+    
+})
+
+app.listen(3000,function(){
+    console.log("server is up");
 })
